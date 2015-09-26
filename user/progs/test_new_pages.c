d9fd9ab9d6a908a8db451716b2145839e0648a90
Shelton George Dsouza <sdsouza@unix3.andrew.cmu.edu>
2015-09-25 23:29:14 -0400

    First Commit: Syscall wrappers working fine

---
+++ user/progs/test_new_pages.c
@@ -0,0 +1,15 @@
+#include<syscall.h>
+#include<simics.h>
+#include<string.h>
+int main()
+{
+	/*void * base = (void*)0xffffcff0;
+	int c = new_pages(base,PAGE_SIZE);
+	lprintf("Added a new page %d and %s \n",c,strerror(c));
+
+	int d = remove_pages(base);
+	lprintf("removed a new page %d and %s \n",d,strerror(d));
+	
+	task_vanish(100);*/
+	return 0;
+}
